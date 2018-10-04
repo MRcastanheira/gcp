e2f7b843f28a4620210ed77da3270cf8392993c2
Matheus Castanheira <matheusrc@outlook.com>
2018-10-03 23:11:47 -0300

    classifying

--- gcp.py
+++ gcp.py
@@ -1,70 +1,83 @@
 import fileinput
 import sys
+import numpy as np
 
-def populationSize 500
+def readFileInstance(file):
+	nodes = 0
+	edges = 0
+	inputEdges = []
+	population = []
 
-def fitness(graph, colors, nodes):
-	# for c in range(colors):
-		# for j in range(graph[j]):
-			# connected = graph[c][j]
-			# if(connected):
-				# if(colors[c] == colors[graph[c][j]]):
-					# print("VIOLACAO");
-	score = 0;
-					
-	for i in range(nodes):
-		for j in range(nodes):
-			if(graph[i][j] == 1):
-				if(colors[i] == colors[j]):
-					print("Warning: {0} ({1}) to {2} ({3})".format(i, colors[i], j, colors[j]));
-					score -= 1;
-		
-	return score
+	for line in fileinput.input(file):
+		if(line[0] == 'p'):
+			params = line.split()
+			nodes = int(params[2])
+			edges = int(params[3])
+		elif (line[0] == 'e'):
+			params = line.split()
+			fromVertex = int(params[1])
+			toVertex = int(params[2])
+			inputEdges.append([fromVertex, toVertex])
 
-nodes = 0
-edges = 0
-inputEdges = list()
-population = list()
+	graph = [0] * nodes
+	for j in range(nodes):
+		graph[j] = [0] * nodes
 
-for line in fileinput.input('simple.col'):
-	if(line[0] == 'p'):
-		params = line.split()
-		nodes = int(params[2]);
-		edges = int(params[3]);	
-	elif (line[0] == 'e'):
-		params = line.split()
-		fromVertex = int(params[1]);
-		toVertex = int(params[2]);
-		inputEdges.append([fromVertex, toVertex])
+	for x in inputEdges:
+		j = x[0] - 1
+		i = x[1] - 1
+		graph[j][i] = 1
+		graph[i][j] = 1
 
+	return graph
 
-graph = [0] * nodes;
-for j in range(nodes):
-	graph[j] = [0] * nodes;
+class Individual:
+	def __init__(self, graph):
+		self.graph = graph
+		self.numNodes = len(graph[0])
+		self.vertexColors = np.random.randint(1, self.numNodes, size=self.numNodes)
 
-for x in inputEdges:
-	j = x[0] - 1;
-	i = x[1] - 1;
-	graph[j][i] = 1 
-	graph[i][j] = 1
-	#print("{0} - {1}", x[0], x[1]);
-	
-print("nodes: {0}, edges: {1}".format(nodes, edges));
+	def fitness(self):
+		score = 0
+		for i in range(self.numNodes):
+			for j in range(self.numNodes):
+				if(self.graph[i][j] == 1):
+					if(self.vertexColors[i] == self.vertexColors[j]):
+						print("Warning: {0} ({1}) to {2} ({3})".format(i, self.vertexColors[i], j, self.vertexColors[j]))
+						score -= 1
+		return score
 
-for j in range(nodes):
-	for i in range(nodes):
-		#print("{0}".format(graph[j][i]));
-		sys.stdout.write(str(graph[j][i]) + " ")
-	print()
-	
+class Population:
+	def __init__(self, graph, size):
+		self.numNodes = len(graph[0])
+		self.graph = graph
+		self.size = size
+		self.population = self.initialize()
 
-# vertex color representation
-graphColors = [0] * nodes;
-#for i in range(populationSize)
-	#graphColors = np.random.randint(1, nodes, size=nodes) #Create random coloration for the graph
-	#population.append(graphColors)
-print(graphColors);
+		for i, individual in enumerate(self.population):
+			print("Node {0} colors: {1}".format(i, individual.vertexColors))
+			individual.fitness()
 
-score = fitness(graph, graphColors, nodes)
+	def initialize(self):
+		population = []
+		for i in range(self.size):
+			individual = Individual(self.graph)
+			population.append(individual)
+		return population
 
-print(score);
+	def beautifulGraph(self):
+		for i in range(self.numNodes):
+			if(i == 0):
+				sys.stdout.write("  ")
+				for x in range(self.numNodes):
+					sys.stdout.write(chr(x + 65) + " ")
+				print()
+			for j in range(self.numNodes):
+				if(j == 0):
+					sys.stdout.write(chr(i + 65) + " ")
+				sys.stdout.write(str(graph[i][j]) + " ")
+			print()
+
+graph = readFileInstance('simple.col')
+population = Population(graph, 1)
+population.beautifulGraph()
